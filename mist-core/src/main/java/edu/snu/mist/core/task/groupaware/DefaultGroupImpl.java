2f363dbf8421df14e978fe866a7387822d0c5cd4
taegeonum <taegeonum@gmail.com>
2018-04-25 20:49:11 +0900

    Disable activeQueryQueue.add

--- mist-core/src/main/java/edu/snu/mist/core/task/groupaware/DefaultGroupImpl.java
+++ mist-core/src/main/java/edu/snu/mist/core/task/groupaware/DefaultGroupImpl.java
@@ -214,11 +214,11 @@ final class DefaultGroupImpl implements Group {
         numProcessedEvent += processedEvent;
 
         query.setReady();
-      } else {
+      } /*else {
         if (query.getGroup() == this) {
           activeQueryQueue.add(query);
         }
-      }
+      }*/
 
       // Reschedule this group if it still has events to process
       if (elapsedTime(startTime) > timeout) {
