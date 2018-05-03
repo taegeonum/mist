876c90a49990271b90cb16eb0e8b55e54ddd5c88
taegeonum <taegeonum@gmail.com>
2018-05-03 11:40:58 +0900

    min resolve thundering herd

--- mist-core/src/main/java/edu/snu/mist/core/master/allocation/MinLoadQueryAllocationManager.java
+++ mist-core/src/main/java/edu/snu/mist/core/master/allocation/MinLoadQueryAllocationManager.java
@@ -22,7 +22,7 @@ import edu.snu.mist.formats.avro.TaskStats;
 import org.apache.reef.tang.annotations.Parameter;
 
 import javax.inject.Inject;
-import java.util.Map;
+import java.util.*;
 
 /**
  * The minimum load query allocation manager.
@@ -39,6 +39,8 @@ public final class MinLoadQueryAllocationManager implements QueryAllocationManag
    */
   private final TaskStatsMap taskStatsMap;
 
+  private final Random random = new Random();
+
   @Inject
   private MinLoadQueryAllocationManager(
       @Parameter(ClientToTaskPort.class) final int clientToTaskPort,
@@ -54,17 +56,30 @@ public final class MinLoadQueryAllocationManager implements QueryAllocationManag
   private IPAddress getMinTaskIpAddress() {
     double minLoad = Double.MAX_VALUE;
     String minTask = null;
+
+    final List<Map.Entry<String, TaskStats>> tasks = new ArrayList<>(taskStatsMap.entrySet().size());
     for (final Map.Entry<String, TaskStats> entry : taskStatsMap.entrySet()) {
-      if (minLoad > entry.getValue().getTaskLoad()) {
-        minLoad = entry.getValue().getTaskLoad();
-        minTask = entry.getKey();
-      }
+      tasks.add(entry);
     }
 
-    if (minTask == null) {
-      throw new RuntimeException("MinTask is null");
+    Collections.sort(tasks, new Comparator<Map.Entry<String, TaskStats>>() {
+      @Override
+      public int compare(final Map.Entry<String, TaskStats> o1, final Map.Entry<String, TaskStats> o2) {
+        if (o1.getValue().getTaskLoad() < o2.getValue().getTaskLoad()) {
+          return -1;
+        } else if (o1.getValue().getTaskLoad() > o2.getValue().getTaskLoad()) {
+          return 1;
+        } else {
+          return 0;
+        }
+      }
+    });
+
+    if (tasks.size() >= 2) {
+      return new IPAddress(tasks.get(random.nextInt(2)).getKey(), clientToTaskPort);
+    } else {
+      return new IPAddress(tasks.get(0).getKey(), clientToTaskPort);
     }
-    return new IPAddress(minTask, clientToTaskPort);
   }
 
   @Override
